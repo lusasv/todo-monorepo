d46cac87d7a61c4fcb9232d2d75e74ced84561d2
PO Agent <po@squad.local>
2026-04-23 13:18:24 +0000

    US-5: implementacao
    Co-Authored-By: FusionCode <noreply@brq.com>

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,5 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
-import TelaPage from "./TelaPage";
 
-createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/tela" element={<TelaPage />} />
-      <Route path="/*" element={<App />} />
-    </Routes>
-  </BrowserRouter>
-);
+createRoot(document.getElementById("root")!).render(<App />);
